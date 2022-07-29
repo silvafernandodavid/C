77abc6b577e050d010d06c72c7df3c0880940ede
silva <silvafernandodavid01@gmail.com>
2022-07-29 19:46:53 -0300

    operaciones

---
+++ MacrosAsignacionOperaciones/asignacionYOperaciones.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  int x, y;
+  double z;
+  x = 10;
+  y = 2;
+
+  x = x + y;
+
+  printf("El valor de x + y = %i\n", x);
+
+  x = x - y;
+
+  printf("El valor de x - y = %i\n", x);
+
+  x = x * y;
+
+  printf("El valor de x * y = %i\n", x);
+
+  x = x / y;
+
+  printf("El valor de x / y = %i\n", x);
+
+  // x = x % y;x = x % y;
+
+
+  // printf("El valor de x resto y = %i\n", x);
+
+
+
+  return 0;
+} 
